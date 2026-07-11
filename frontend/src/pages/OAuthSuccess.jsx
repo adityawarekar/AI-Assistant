fe9bc61e43e08831b88c5d6651728ebe1e092985
Aditya Warekar <adityawarekar1984@gmail.com>
2026-07-11 21:27:05 +0530

    Remove all logs

--- frontend/src/pages/OAuthSuccess.jsx
+++ frontend/src/pages/OAuthSuccess.jsx
@@ -9,55 +9,32 @@ const OAuthSuccess = () => {
 
   useEffect(() => {
     const handleGoogleLogin = async () => {
-      console.log("========== GOOGLE LOGIN ==========");
-
       const params = new URLSearchParams(window.location.search);
-
       const token = params.get("token");
 
-      console.log("STEP 1: Token =", token);
-
       if (!token) {
-        console.log("No token found");
+        navigate("/", { replace: true });
         return;
       }
 
       try {
-        console.log("STEP 2: Calling /auth/me...");
-
         const res = await API.get("/auth/me", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
 
-        console.log("STEP 3: /auth/me success");
-        console.log(res.data);
-
-        console.log("Before setAuth");
         setAuth(res.data, token);
-        console.log("After setAuth");
 
-        
-
-        console.log("STEP 4: setAuth completed");
-        console.log("Before navigate");
-        navigate("/dashboard");
-        console.log("After navigate");
-
-        console.log("STEP 5: navigate called");
-
+        navigate("/dashboard", { replace: true });
       } catch (error) {
-        console.error("GOOGLE LOGIN ERROR");
         console.error(error);
-        console.error(error.response?.data);
-
         navigate("/", { replace: true });
       }
     };
 
     handleGoogleLogin();
-  }, []);
+  }, [navigate, setAuth]);
 
   return (
     <div className="min-h-screen flex items-center justify-center">
